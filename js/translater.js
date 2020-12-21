1345253c5cbcc60cf08b8e232a0b7d2818aa7ac4
removio <76274060+removio@users.noreply.github.com>
2020-12-21 05:50:26 +0200

    1

---
+++ js/translater.js
@@ -0,0 +1,229 @@
+var new_lang = {
+    "lt0": "XX",
+
+    "lt1": "Sbarazzati di verruche e papillomi in modo veloce e sicuro!",
+    
+    "lt2": "OFFERTA SPECIALE",
+    
+    "lt3": "al termine della promozione è rimasto <div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt4": "Prezzo normale",
+    
+    "lt5": "Prezzo promozionale",
+    
+    "lt6": "Ordinare",
+    
+    "lt7": "EFFETTO",
+    
+    "lt8": "Azione efficace contro <br>diversi ceppi<br> del papillomavirus umano",
+    
+    "lt9": "nell'arco di 48 ore",
+    
+    "lt10": "Annienta completamente <br>il fattore scatenante nell'arco<br> delle prime 48 ore",
+    
+    "lt11": "Cura complessiva",
+    
+    "lt12": "Blocca l'attività del virus <br>e potenzia le difese<br> immunitarie",
+    
+    "lt13": "SBARAZZATI DEI PAPILLOMI E PREVIENI IL CANCRO DELLA PELLE! <br><span>REMOVIOGEL</span> SALVA LA VITA!",
+    
+    "lt14": "La combinazione dei principi attivi elimina nuove formazioni cutanee e impedisce loro di degenerare in tumori maligni.",
+    
+    "lt15": "Papillomi<br> sul collo",
+    
+    "lt16": "Papillomi <br>nel cavo ascellare<br>",
+    
+    "lt17": "Papillomi <br>in stato<br> degenerativo sulle gambe",
+    
+    "lt18": "Papillomi<br> diventati cancro",
+    
+    "lt19": "Motivi che causano la comparsa dei papillomi",
+    
+    "lt20": "I papillomi sono escrescenze sulla pelle causate da un'infezione virale, chiamata anche papillomavirus umano o HPV. Si trasmette esclusivamente da persona a persona attraverso ferite superficiali sulla pelle. Il periodo di incubazione di questa malattia varia da diversi giorni a decine di anni. Non appena il corpo si indebolisca a causa di qualche patologia, di stress, di gravidanza nelle donne, i papillomi compaiono sulla pelle",
+    
+    "lt21": "<span>Removiogel</span> combatte con tutti i tipi di papillomavirus umano",
+    
+    "lt22": "<span style=\"color: #0158ff;\">Papillomi semplici (vulgares)</span> Di regola compaiono sugli arti superiori",
+    
+    "lt23": "<span style=\"color: #127abe;\">Papillomi piatti</span> Si diffondono sul collo, il volto,<br> gli arti superiori",
+    
+    "lt24": "<span style=\"color: #0ba1e3;\">Papillomi plantari</span> Colpiscono le piante dei piedi",
+    
+    "lt25": "<span style=\"color: #1dc1dd;\">Papillomi filiformi</span> Localizzazione della comparsa: collo, volto, aree pubiche ed<br> ascellari",
+    
+    "lt26": "<span style=\"color: #e2000f;\">Condilomi</span> Chiamati anche papillomi acuminati<br>: un gruppo estremamente pericoloso",
+    
+    "lt27": "SBARAZZATENE AL PIÙ PRESTO!",
+    
+    "lt28": "Il gel <span>Removiogel</span> è un metodo semplice ed efficace per sbarazzarsi di papillomi e verruche!",
+    
+    "lt29": "Sopprime l'attività delle cellule virali, penetrando negli strati profondi del derma",
+    
+    "lt30": "Rimuove in maniera indolore le neoplasie, prevenendo l'ulteriore sviluppo di papillomi",
+    
+    "lt31": "Ripristina le zone cutanee danneggiate",
+    
+    "lt32": "Aumenta la resistenza ai virus, alle infezioni fungine, previene lo sviluppo del cancro",
+    
+    "lt33": "OFFERTA SPECIALE",
+    
+    "lt34": "al termine della promozione è rimasto <div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt35": "Prezzo normale",
+    
+    "lt36": "Prezzo promozionale",
+    
+    "lt37": "Ordinare",
+    
+    "lt38": "ORDINARE",
+    
+    "lt39": "Il gel <span>Removiogel</span> è un metodo semplice ed efficace per sbarazzarsi di papillomi e verruche!",
+    
+    "lt40": "Olio di zenzero",
+    
+    "lt41": "Possiede potenti proprietà antibatteriche e antinfiammatorie, che permettono di prevenire la diffusione del virus",
+    
+    "lt42": "Estratto di consolida maggiore",
+    
+    "lt43": "Usato in medicina per curare<br> malattie infiammatori. Elimina rapidamente e<br> senza dolore verruche e papillomi",
+    
+    "lt44": "Estratto di melissa",
+    
+    "lt45": "Possiede una spiccata azione antivirale. Migliora la produzione di collagene ed elastina, che sono proteine essenziali per la salute della pelle",
+    
+    "lt46": "Tocoferolo",
+    
+    "lt47": "Ha proprietà antinfiammatorie e antisettiche, aiuta a ripristinare l'epidermide, nutre la pelle con vitamine e microelementi",
+    
+    "lt48": "Estratto di Lemon Grass",
+    
+    "lt49": "Grazie alla sua ricca composizione chimica, è efficace nel trattamento di cicatrici e macchie della pelle. Elimina l'infiammazione, migliora l'immunità locale",
+    
+    "lt50": "Olio dell'albero del tè",
+    
+    "lt51": "Arresta il nutrimento di papillomi e verruche, garantendo, senza effetti collaterali, l'estinzione delle neoplasie cutanee e prevenendo la comparsa di nuove",
+    
+    "lt52": "Cannella",
+    
+    "lt53": "Ha un forte effetto antivirale, previene la comparsa del cancro, ferma la scissione cellulare patogena",
+    "lt54": "Niacinamide",
+    
+    "lt55": "Rafforza l'immunità locale Ha un effetto cicatrizzante e battericida, fornendo una guarigione rapida e indolore.",
+    
+    "lt56": "<span>Removiogel</span> è la tua nuova vita senza papillomi e verruche!",
+    
+    "lt57": "Azione precisa e localizzata",
+    
+    "lt58": "Distrugge i papillomi insieme alla radice",
+    
+    "lt59": "Libera dal virus",
+    
+    "lt60": "Senza lasciare tracce e in maniera indolore",
+    
+    "lt61": "Azione delicata",
+    
+    "lt62": "Non danneggia la pelle,<br>ricostruisce i tessuti",
+    
+    "lt63": "ORDINARE",
+    
+    "lt64": "Studi clinici ",
+    
+    "lt65": "Nel 2019, gli specialisti dell'Istituto di Dermatovenereologia hanno condotto studi clinici di <span>Removiogel</span> su un gruppo di 658 volontari affetti da HPV e con papillomi e verruche sul corpo. Sono stati ottenuti i seguenti dati:",
+    
+    "lt66": "Rimozione di formazioni cutanee (papillomi, condilomi genitali, verruche)",
+    
+    "lt67": "Eliminazione del virus causa di ricadute (risultati dopo 3 mesi)",
+    
+    "lt68": "Il 99% dei testati non ha avuto effetti collaterali",
+    
+    "lt69": "II parere dello specialista",
+    
+    "lt70": "Quasi ogni persona ha papillomi e verruche. Si tratta di escrescenze benigne sul corpo. Ma la loro insidiosità sta nel fatto che prima o poi possono diventare maligne e portare a malattie oncologiche. Pertanto, i papillomi devono essere rimossi, anche se non ti danno fastidio dal punto di vista estetico. <br>Ai miei pazienti consiglio l'uso del gel <span>Removiogel</span>. È il rimedio più efficace e sicuro disponibile al momento. Removiogel combatte papillomi e verruche negli strati più profondi della pelle, eliminando completamente le neoplasie indesiderate",
+    
+    "lt71": "Marco Alga.<br>Dermatovenereologo",
+    
+    "lt72": "Commenti dei clienti",
+    
+    "lt73": "Alessio, 50 anni",
+    
+    "lt74": "Ho i papillomi fin dall'infanzia, ma non ho mai attribuito loro molta importanza. In generale, credevo che si trattasse di una specie di nei e non fosse affatto necessario combatterli. Mia moglie mi ha fatto capire che non si tratta affatto di nei ed è possibile l'insorgenza di neoplasie oncologiche. Su sua insistente richiesta, sono andato da un dottore, che mi ha prescritto <span>Removiogel</span>. I papillomi sono passati velocemente e senza dolore, senza lasciare alcuna traccia",
+    
+    "lt75": "Luciana, 35 anni",
+    
+    "lt76": "Il mio compagno mi ha infettato con verruche genitali. Siamo andati a curarci insieme da un medico, il quale ha detto che il nostro virus è altamente oncogeno, cioè è altamente probabile che porti allo sviluppo del cancro. Ci siamo tanto spaventati! Ci sono stati prescritti moltissimi preparati, ma i risultati sono stati insignificanti. <span>Removiogel</span> me l'ha fatto conoscere un'amica a cui avevo confidato il mio problema, e ho deciso di provarlo. Per un mese abbiamo applicato questo gel sulle aree problematiche, tutto è passato abbastanza velocemente. Dopo due mesi abbiamo fatto le analisi: completamente guariti!",
+    
+    "lt77": "Maria, 43 anni",
+    
+    "lt78": "Mi sono comparse le verruche circa cinque anni fa. Non si sa quello che ho provato per combatterle: ho applicato unguenti, le ho cauterizzate, le ho rimosse chirurgicamente. Nessun metodo si è rivelato efficace. Ho trovato per caso una soluzione su Internet: mi sono imbattuta in un articolo su <span>Removiogel.</span> L'ho ordinato e l'ho provato. Dopo due settimane, ho notato che le verruche avevano cominciato a farsi nere, per poi cadere completamente. Entro un mese dal primo utilizzo, la pelle si è fatta assolutamente liscia, non è rimasta una sola cicatrice",
+    
+    "lt79": "Alessio, 50 anni",
+    
+    "lt80": "Ho i papillomi fin dall'infanzia, ma non ho mai attribuito loro molta importanza. In generale, credevo che si trattasse di una specie di nei e non fosse affatto necessario combatterli. Mia moglie mi ha fatto capire che non si tratta affatto di nei ed è possibile l'insorgenza di neoplasie oncologiche. Su sua insistente richiesta, sono andato da un dottore, che mi ha prescritto <span>Removiogel</span>. I papillomi sono passati velocemente e senza dolore, senza lasciare alcuna traccia",
+    
+    "lt81": "Luciana, 35 anni",
+    
+    "lt82": "Il mio compagno mi ha infettato con verruche genitali. Siamo andati a curarci insieme da un medico, il quale ha detto che il nostro virus è altamente oncogeno, cioè è altamente probabile che porti allo sviluppo del cancro. Ci siamo tanto spaventati! Ci sono stati prescritti moltissimi preparati, ma i risultati sono stati insignificanti. <span>Removiogel</span> me l'ha fatto conoscere un'amica a cui avevo confidato il mio problema, e ho deciso di provarlo. Per un mese abbiamo applicato questo gel sulle aree problematiche, tutto è passato abbastanza velocemente. Dopo due mesi abbiamo fatto le analisi: completamente guariti!",
+    
+    "lt83": "Maria, 43 anni",
+    
+    "lt84": "Mi sono comparse le verruche circa cinque anni fa. Non si sa quello che ho provato per combatterle: ho applicato unguenti, le ho cauterizzate, le ho rimosse chirurgicamente. Nessun metodo si è rivelato efficace. Ho trovato per caso una soluzione su Internet: mi sono imbattuta in un articolo su <span>Removiogel.</span> L'ho ordinato e l'ho provato. Dopo due settimane, ho notato che le verruche avevano cominciato a farsi nere, per poi cadere completamente. Entro un mese dal primo utilizzo, la pelle si è fatta assolutamente liscia, non è rimasta una sola cicatrice",
+    
+    "lt85": "Alessio, 50 anni",
+    
+    "lt86": "Ho i papillomi fin dall'infanzia, ma non ho mai attribuito loro molta importanza. In generale, credevo che si trattasse di una specie di nei e non fosse affatto necessario combatterli. Mia moglie mi ha fatto capire che non si tratta affatto di nei ed è possibile l'insorgenza di neoplasie oncologiche. Su sua insistente richiesta, sono andato da un dottore, che mi ha prescritto <span>Removiogel</span>. I papillomi sono passati velocemente e senza dolore, senza lasciare alcuna traccia",
+    
+    "lt87": "Removiogel",
+    
+    "lt88": "Sbarazzati di verruche e papillomi in modo veloce e sicuro!",
+    
+    "lt89": "OFFERTA SPECIALE",
+    
+    "lt90": "al termine della promozione è rimasto <div class=\"form-header__timer-script\"><span class=\"hoursSH\">11</span>:<span class=\"minutesSH\">11</span>:<span class=\"secondsSH\">11</span></div>",
+    
+    "lt91": "Prezzo normale",
+    
+    "lt92": "Prezzo promozionale",
+    
+    "lt93": "Ordinare",
+    
+    "lt94": "EFFETTO",
+    
+    "lt95": "Azione efficace contro <br>diversi ceppi<br> del papillomavirus umano",
+    
+    "lt96": "nell'arco di 48 ore",
+    
+    "lt97": "Annienta completamente <br>il fattore scatenante nell'arco<br> delle prime 48 ore",
+    
+    "lt98": "Cura complessiva",
+    
+    "lt99": "Blocca l'attività del virus <br>e potenzia le difese<br> immunitarie",
+    
+    "lt100": "ORDINARE in tre passi",
+    
+    "lt101": "PRIMO PASSO",
+    
+    "lt102": "Lasciare una richiesta d’ordine<br> sul nostro sito",
+    
+    "lt103": "SECONDO PASSO",
+    
+    "lt104": "Un operatore ti contatterà<br> per<br> confermare l'ordine",
+    
+    "lt105": "TERZO PASSO",
+    
+    "lt106": "Pagamento online o<br> al momento<br> della consegna"
+        
+    
+
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
